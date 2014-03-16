8e49a5dc2af8feaf254625a8d179bf891a47888b
alicjasalamon <alicjasalamon@gmail.com>
2014-03-16 19:26:12 +0100

    hover photo + refactor

---
+++ Portfolio/Portfolio/js/imageHover.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    $('#photo').mousemove(function (e) {
+        var r = 175;
+
+        var x = $('#photo').offset().left + r;
+        var y = $('#photo').offset().top + r;
+
+        var ex = e.pageX;
+        var ey = e.pageY;
+
+        var xx = (ex - x);
+        var yy = (ey - y);
+
+        if (xx * xx + yy * yy < r * r)
+            $("#photo").attr('src', "images/photo2.png");
+        else
+            $("#photo").attr('src', "images/photo.png");
+    });
+
+    $('#photo').mouseleave(function (e) {
+        $("#photo").attr('src', "images/photo.png");
+    });
+
+
+});
+
